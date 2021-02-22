6d9c7f6a7fd8a24b2744be55eb93e675680d2281
Pierre BRESSY <pierre.bressy@heig-vd.ch>
2021-02-22 12:30:47 +0100

    update

---
+++ TDyyyymmdd.c
@@ -0,0 +1,16 @@
+/**
+  \file      TDyyyymmdd.c
+  \brief     for test
+  \author    Pierre BRESSY
+  \version   1.0
+  \date      2021-01-11 13:55:00
+  \details
+
+**/
+
+#include <stdio.h>  // standard library for inputs and ouputs
+
+int main(int argc, const char *argv[]) {
+  printf("Hello World \n");
+  return 0;
+}
